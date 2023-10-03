a9d5e59c7ab1aa8493e244d37d8fd8d81aa719c8
TimoKuukka <46355@edu.raseko.fi>
2023-10-03 15:19:33 +0300

    Added logging module and used it in microservices

--- microservice.js
+++ microservice.js
@@ -10,63 +10,79 @@ const cron = require('node-cron');
 // Home made library to access price API from porssisahko.net
 const getPrices = require('./getNewPrices');
 
+// Home made library to add messages to a log file
+const logger = require('./logger')
+
 // APP SETTINGS
 // ------------
 
 // Create a new pool for Postgres connections
 const pool = new Pool({
-  user: 'postgres', // In production allways create a new user for the app
+  user: 'postgres', // In production always create a new user for the app
   password: 'Q2werty',
   host: 'localhost', // Or localhost or 127.0.0.1 if in the same computer
-  database: 'smarthome2',
+  database: 'smarthome',
   port: 5432
 });
 
 // GET, PROCESS AND SAVE DATA
 // --------------------------
 
-// Use a date variable to keep track of successfull data retrievals
-let lastFethcedDate = '1.1.2023'; // Initial value, in production use settings file
-
+// Use a date variable to keep track of successful data retrievals
+let lastFetchedDate = '1.1.2023'; // Initial value, in production use settings file
+let message = ''
+const logFile = 'dataOperationsLog'
 // Try to run an operation in 5 minute intervals from 3 to 4 PM
 cron.schedule('*/5 15 * * *', () => {
   try {
     let timestamp = new Date(); // Get the current timestamp
-    let dateStr = timestamp.toLocaleDateString(); // Take datepart of the timestamp
+    let dateStr = timestamp.toLocaleDateString(); // Take date part of the timestamp
 
-    // If the date of last sucessfull fetch is not the current day, fetch data
-    if (lastFethcedDate != dateStr) {
-      console.log('Started fething price data ');
-      // TODO: add this entry to the log file
+    // If the date of last successful fetch is not the current day, fetch data
+    if (lastFetchedDate != dateStr) {
+      message = 'Started fetching price data'
+      console.log(message);
+      logger.add2log(message, logFile)
       getPrices.fetchLatestPriceData().then((json) => {
 
-        //loop trough prices data and pick starDate and price elements
+        // Loop through prices data and pick startDate and price elements
         json.prices.forEach(async (element) => {
           let values = [element.startDate, element.price];
 
           // Build a SQL clauset to insert values into table
           const sqlClause = 'INSERT INTO public.hourly_price VALUES ($1, $2) ON CONFLICT DO NOTHING RETURNING *'; 
-          // Function for running SQL operations asyncroneously
+          // Function for running SQL operations asyncronously
           const runQuery = async () => {
             let resultset = await pool.query(sqlClause, values);
             return resultset;
           }
-          // Call queryfunction and echo results to console
-          runQuery().then((resultset) => console.log(resultset.rows[0]))
-          // TODO: add this entry to the log file
+          // Call query function and echo results to console
+          runQuery().then((resultset) => {
+            if (resultset.rows[0] != undefined ) {
+              message = 'Added a row'
+            }
+            else {
+              message = 'Skipped an existing row'
+            }
+            console.log(message);
+            logger.add2log(message, logFile)
+
+          })
+          
         });
       });
-      lastFethcedDate = dateStr; // Set fetch date to current date
-      console.log('Fethed at', lastFethcedDate)
-      // TODO: add this entry to the log file
+      lastFetchedDate = dateStr; // Set fetch date to current date
+      message = 'Fetched at ' + lastFetchedDate;
+      console.log(message)
+      logger.add2log(message, logFile)
     } else {
-      console.log('Data has been successfully retrieved earlier today');
-      // TODO: add this entry to the log file
+      message = 'Next scheduled event: Data has been successfully retrieved earlier today'
+      console.log(message);
+      logger.add2log(message, logFile)
     }
   } catch (error) {
-    console.log('An error() occurred, trying again in 5 minutes until 4 PM');
-    // TODO: add this entry and the error message to the log file
+    message = 'An error occurred (' + error.toString() + '), trying again in 5 minutes until 4 PM';
+    console.log(message)
+    logger.add2log(message, logFile)
   }
 });
-
-// TODO: Create a function to do this ie. create log message and timestamp.
