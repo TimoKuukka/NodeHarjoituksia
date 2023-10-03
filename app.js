70846f1f1394f138b2003574c0e61214f75fd0d5
TimoKuukka <46355@edu.raseko.fi>
2023-10-03 10:42:20 +0300

    Route to hourly chart page, bar chart

--- app.js
+++ app.js
@@ -4,12 +4,18 @@
 // LIBRARIES AND MODULES
 // ---------------------
 
-
 // Use Express as web engine
 const express = require('express');
 // Use Express Handlebars as template engine
-const {engine} = require('express-handlebars');
-const req = require('express/lib/request');
+const { engine } = require('express-handlebars');
+
+/* Get external data with node-fetch for version 2.x
+This version should be installed as follows: npm install node-fetch@2 
+const fetch = require('node-fetch');*/
+
+/* Get external data with node-fetch for version 3.x
+import fetch from 'node-fetch'; */
+
 
 // EXPRESS APPLICATION SETTINGS
 // ----------------------------
@@ -18,7 +24,7 @@ const req = require('express/lib/request');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Set folder paths: public is for assets and views for pages
+// Set folder paths: public is for assets and views is for pages
 app.use(express.static('public'));
 app.set('views', './views');
 
@@ -30,16 +36,14 @@ app.set('view engine', 'handlebars');
 // ----------
 
 
-// TODO: Add date and time as dynamic data for the homepage, is it sensible to use server for creating time values?
-
-// Route to home page
+// Route to home page 
 app.get('/', (req, res) => {
-    
+
     // Handlebars needs a key to show data on a page, json is a good way to send it
     let homePageData = {
         'price': 31.25,
         'wind': 2,
-        'temperature': 18,
+        'temperature': 18
     };
 
     // Render index.handlebars and send dynamic data to the page
@@ -47,29 +51,73 @@ app.get('/', (req, res) => {
 
 });
 
-
 // Route to hourly data page
-app.get('/hourly',(req, res) => {
-    
+app.get('/hourly', (req, res) => {
+
     // Data will be presented in a table. To loop all rows we need a key for table and for column data
-    let hourlyPageData = { 'tableData': [
-    {'hour': 13,
-    'price': 31.25},
-    {'hour': 14,
-    'price': 32.10},
-    {'hour': 15,
-    'price': 30.11},
-    {'hour': 16,
-    'price': 29.44}
-    ]
-};
+    let hourlyPageData = {
+        'tableData': [
+            {
+                'hour': 13,
+                'price': 31.44
+            },
+            {
+                'hour': 14,
+                'price': 32.10
+            },
+            {
+                'hour': 15,
+                'price': 30.50
+            },
+            {
+                'hour': 16,
+                'price': 29.99
+            }
+        ]
+    };
+
     res.render('hourly', hourlyPageData)
+
 });
 
+// Route to hourly chart page
+app.get('/chart', (req, res) => {
+
+    // Data will be presented in a bar chart. Data will be sent as JSON array to get it work on handlebars page
+    let tableHours = [12, 13, 14, 15, 16];
+    let jsonTableHours = JSON.stringify(tableHours)
+    let tablePrices = [10, 8, 10, 12, 15];
+    let jsonTablePrices = JSON.stringify(tablePrices)
+    let chartPageData = { 'hours': jsonTableHours, 'prices': jsonTablePrices };
+
+    res.render('chart', chartPageData)
+
+});
+
+app.get('/test', (req, res) => {
+
+    // Data will be presented in a bar chart. Data will be sent as JSON array
+    let tableHours = [12, 13, 14, 15, 16];
+    let jsonTableHours = JSON.stringify(tableHours)
+    let tablePrices = [10, 8, 10, 12, 15];
+    let jsonTablePrices = JSON.stringify(tablePrices)
+    let chartPageData = { 'hours': jsonTableHours, 'prices': jsonTablePrices };
+
+    res.render('testCJSv4', chartPageData)
+
+});
+
+app.get('/callback', (req, res) => {
+
+    let priceData = {
+        'retailPrice': 24.05,
+        'taxMultiplier': 1.24
+    }
+
+    res.render('callbackesim', priceData);
+
+});
 
 // START THE LISTENER
 app.listen(PORT);
 console.log('Server started and it will listen TCP port', PORT);
-
-
-
