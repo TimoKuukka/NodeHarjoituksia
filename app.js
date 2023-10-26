cfc13839729cfe5c2115c634d9f2869482043557
TimoKuukka <46355@edu.raseko.fi>
2023-10-26 11:53:26 +0300

    changed hinta to price

--- app.js
+++ app.js
@@ -42,13 +42,13 @@ app.get('/', (req, res) => {
     };
 
     cprice.getCurrentPrice().then((resultset) => {
-        console.log(resultset.rows[0])
-        // FIXME: in database "hinta" on index.handlebars "price"
-        homePageData.price = (resultset.rows[0])
-    })
 
-    // Render index.handlebars and send dynamic data to the page
-    res.render('index', homePageData)
+        // Set the price value to the data object
+        homePageData.price = resultset.rows[0]['price']
+        
+        // Render index.handlebars and send dynamic data to the page
+        res.render('index', homePageData)
+    })
 
 });
 
